57e1514971c293cf284367e8cefa0b8bf75b769a
André Villela <dede.villela@gmail.com>
2017-12-30 20:59:42 -0200

    Added /** global: VRS */ [scrutinizer]

--- CustomAircraftMarkers.js
+++ CustomAircraftMarkers.js
@@ -1,3 +1,4 @@
+/** global: VRS */
 if (typeof VRS === "undefined" && VRS.globalDispatch && VRS.serverConfig) {
 	VRS.globalDispatch.hook(VRS.globalEvent.bootstrapCreated, function() {
 		if (VRS.globalOptions && VRS.globalOptions.aircraftMarkers) {
